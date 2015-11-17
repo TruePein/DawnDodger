fa1c9493ff0117b19008d26a061f221f6d8b6fd8
Eric Stones <stoneseric_21@ymail.com>
2015-11-16 23:10:30 -0800

    Initial Commit

---
+++ Dawn Dodger/Assets/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+    private Transform player;
+
+    public Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.position + offset;
+	}
+}
---
+++ Dawn Dodger/Assets/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float maxSpeed = 10f;
+    bool facingRight = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float hMove = Input.GetAxis("Horizontal");
+        float vMove = Input.GetAxis("Vertical");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(hMove * maxSpeed, vMove * maxSpeed);
+        if (hMove > 0 && !facingRight)
+        {
+            Flip();
+        }else if (hMove < 0 && facingRight)
+        {
+            Flip();
+        }
+	}
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
